de6726184cf0594f4b5562292afe3b1cc432d9ad
Kirsten Kustusch <kirsten.kustusch@gmail.com>
2019-12-02 22:17:40 +0200

    added patch request and tests

---
+++ src/BookmarksContext.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BookmarksContext = React.createContext({
+  bookmarks: [],
+  addBookmark: () => {
+
+  },
+  deleteBookmark: () => {
+
+  },
+  updateBookmark: () => {
+    
+  }
+})
+
+export default BookmarksContext
